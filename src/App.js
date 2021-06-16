2f3cfaa03016e11430ba2cbf54c10d42b8364aa5
bagal-co <withbob2@gmail.com>
2021-06-16 20:26:00 +1000

    added the mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
-import CheckOutPage from './pages/checkout/checkout.component'
+import CheckOutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component.jsx';
 import SignInPage from './pages/signIn-page/signIn-page.component.jsx';
 import SignUpPage from './pages/signUp-page/signUp-page.component';
@@ -21,63 +21,70 @@ import { selectCurrentUser } from './redux/user/user.selector';
 
 import Footer from './components/footer/footer.component';
 
-
 class App extends React.Component {
-  
-  unsubscribeFromAuth = null;
+	unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
+	componentDidMount() {
+		const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
-        });
-      }
+				userRef.onSnapshot(snapShot => {
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data(),
+					});
+				});
+			}
 
-      setCurrentUser(userAuth);
-    });
-  }
+			setCurrentUser(userAuth);
+		});
+	}
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 
-
-  render(){
-    return(
-      <div className="App">
-        <Router>
-        <Header />
-          <Switch>
-            <Route exact path='/' component={HomePage}/>
-            <Route path='/shop' component={ShopPage}/>
-            <Route path='/contact' component={ContactPage}/>
-            <Route exact path='/checkout' component={CheckOutPage}/>
-            <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignInPage />)}/>
-            <Route exact path='/signup' render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignUpPage />)}/>
-          </Switch>
-          <Footer/>
-        </Router>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Router>
+					<Header />
+					<Switch>
+						<Route exact path='/' component={HomePage} />
+						<Route path='/shop' component={ShopPage} />
+						<Route path='/contact' component={ContactPage} />
+						<Route exact path='/checkout' component={CheckOutPage} />
+						<Route
+							exact
+							path='/signin'
+							render={() =>
+								this.props.currentUser ? <Redirect to='/' /> : <SignInPage />
+							}
+						/>
+						<Route
+							exact
+							path='/signup'
+							render={() =>
+								this.props.currentUser ? <Redirect to='/' /> : <SignUpPage />
+							}
+						/>
+					</Switch>
+					<Footer />
+				</Router>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
+	currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user)),
+	setCurrentUser: user => dispatch(setCurrentUser(user)),
 });
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/contact-us/contact.component.jsx
+++ src/components/contact-us/contact.component.jsx
@@ -4,75 +4,103 @@ import FormInput from '../../components/form-input/form-input.component';
 import Button from '../../components/custom-button/button.component';
 import { FaPhone, FaEnvelope, FaLocationArrow } from 'react-icons/fa';
 
-class Contact extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      textarea: ''
-    }
+class Contact extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			firstName: '',
+			lastName: '',
+			email: '',
+			textarea: '',
+		};
+	}
+	handleSubmit = e => {
+		e.preventDefault();
+		const { firstName, lastName, email, textarea } = this.state;
+		this.setState({
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			textarea: textarea,
+		});
+	};
 
-  }
-    handleSubmit = (e) => {
-      e.preventDefault();
-      const { firstName, lastName, email, textarea } = this.state;
-      this.setState({ firstName: firstName, lastName: lastName, email: email, textarea: textarea});
-    }
+	handleChange = e => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	};
+	render() {
+		return (
+			<div className='contact'>
+				<div className='container contact-row'>
+					<div class='contact__details'>
+						<div class='phone'>
+							<div>
+								<FaPhone />
+							</div>
+							<div>
+								<h3>Phone</h3>
+								<p>(+61) 1800 45 645</p>
+							</div>
+						</div>
 
-    handleChange = (e) => {
-      const { name, value } = e.target;
-      this.setState({ [name]: value });
-    }
-  render(){
-    return(
-      <div className="contact">
-        <div className="container contact-row">
-        <div class="contact__details">
-          <div class="location">
-            <div><FaLocationArrow /></div>
-            <div>
-              <h3>Location</h3>
-              <p>A 104 George Street, Beenleigh QLD</p>
-            </div>
-          </div>
-          <div class="email">
-            <div><FaEnvelope /></div>
-            <div>
-              <h3>Email</h3>
-              <p>example@gamil.com</p>
-            </div>
-          </div>
-          <div class="phone">
-            <div><FaPhone /></div>
-            <div>
-              <h3>Phone</h3>
-              <p>(+61) 1800 45 645</p>
-            </div>
-          </div>
-        </div>
+						<div class='email'>
+							<div>
+								<FaEnvelope />
+							</div>
+							<div>
+								<h3>Email</h3>
+								<p>example@gamil.com</p>
+							</div>
+						</div>
 
-        <form action="#" className="contact-form" onSubmit={this.handleSubmit}>
-        <h1>Contact Us</h1>
-          <FormInput type="text" name="firstName" value={this.state.firstName} 
-          onChange={this.handleChange} label="First Name"/>
+						<div class='location'>
+							<div>
+								<FaLocationArrow />
+							</div>
+							<div>
+								<h3>Location</h3>
+								<p>A 104 George Street, Beenleigh QLD</p>
+							</div>
+						</div>
+					</div>
 
-          <FormInput type="text" name="lastName" value={this.state.lastName} 
-          onChange={this.handleChange} label="Last Name"/>
-          <FormInput type="email" name="email" value={this.state.email} 
-          onChange={this.handleChange} label="Email"/>
-          <label htmlFor="textare">Textarea</label>
-          <textarea onChange={this.handleChange}></textarea>
-          <Button type="submut">Send Message</Button>
-        </form>
+					<form
+						action='#'
+						className='contact-form'
+						onSubmit={this.handleSubmit}
+					>
+						<h1>Contact Us</h1>
+						<FormInput
+							type='text'
+							name='firstName'
+							value={this.state.firstName}
+							onChange={this.handleChange}
+							label='First Name'
+						/>
 
-        </div>
-      
-      
-      </div>
-    );
-  };
-};
+						<FormInput
+							type='text'
+							name='lastName'
+							value={this.state.lastName}
+							onChange={this.handleChange}
+							label='Last Name'
+						/>
+						<FormInput
+							type='email'
+							name='email'
+							value={this.state.email}
+							onChange={this.handleChange}
+							label='Email'
+						/>
+						<label htmlFor='textare'>Textarea</label>
+						<textarea onChange={this.handleChange}></textarea>
+						<Button type='submut'>Send Message</Button>
+					</form>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Contact;
